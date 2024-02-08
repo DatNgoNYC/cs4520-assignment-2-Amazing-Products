9ae1608bd513ab69e32e7e4e6b556ff036055ae8
Dat Ngo <datngonyc@gmail.com>
2024-02-08 16:28:07 -0500

    finish final draft version 1.0

--- app/src/main/java/com/cs4520/assignment1/ProductAdapter.kt
+++ app/src/main/java/com/cs4520/assignment1/ProductAdapter.kt
@@ -1,7 +1,9 @@
 package com.cs4520.assignment1
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.cs4520.assignment1.databinding.ProductListItemBinding
 
@@ -26,15 +28,23 @@ class ProductAdapter(private val productList: List<Product>) : RecyclerView.Adap
                 productName.text = product.name
                 productPrice.text = String.format("$%.2f", product.price)
 
+                val backgroundColor = when (product) {
+                    is Product.Food -> R.color.light_yellow
+                    is Product.Equipment -> R.color.light_red
+                }
+
+                // Directly use the binding object's root, which is the ConstraintLayout
+                root.setBackgroundColor(ContextCompat.getColor(root.context, backgroundColor))
+
                 when (product) {
                     is Product.Food -> {
                         productExpiryDate.visibility = View.VISIBLE
                         productExpiryDate.text = "Expires on: ${product.expiryDate}"
-                        productImage.setImageResource(R.drawable.ic_food)
+                        productImage.setImageResource(R.drawable.food)
                     }
                     is Product.Equipment -> {
                         productExpiryDate.visibility = View.GONE
-                        productImage.setImageResource(R.drawable.ic_equipment)
+                        productImage.setImageResource(R.drawable.equipment)
                     }
                 }
             }
